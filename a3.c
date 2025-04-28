570544b603baadd275bfd0551824e87d684875c9
Vera Bu <opilki-in-the-head@mail.ru>
2025-04-28 16:41:20 +0300

    renamed sum3 -> a3

---
+++ a3.c
@@ -0,0 +1,20 @@
+/*
+ * sum3.c
+ * 
+ * Copyright 2025 Bukhalkina.V <Bukhalkina.V@RGL5S72NF2>
+ * 
+ * 
+ * 
+ */
+
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	int x,y,z;
+    scanf("%d %d %d", &x, &y, &z);
+    printf("%d+%d+%d=%d", x,y,z,x+y+z);
+    return 0;
+}
+
