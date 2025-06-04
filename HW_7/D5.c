c385e1823ab20b673e2777b8cb471abe3b7ca1ce
«Vera <opilki-in-the-head@mail.ru>
2025-06-04 19:22:15 +0300

    added d5

---
+++ HW_7/D5.c
@@ -0,0 +1,27 @@
+/*
+ * D5.c
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+#include <stdio.h>
+
+int toBin(int n)
+{
+    if (n == 0) 
+        return 0; 
+    else
+        return (n % 2 + 10 * toBin(n / 2));
+}
+
+int main()
+{
+	int num;
+	scanf("%d", &num);
+	printf("%d\n", toBin(num));
+	return 0;
+}
+
